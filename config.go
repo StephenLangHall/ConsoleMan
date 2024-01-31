f09a1ceeba9e1256319e8c39efe694c723013b1c
step <stephenlh2007@gmail.com>
2024-01-31 07:25:19 -0500

    Move timer updator to func

--- config.go
+++ config.go
@@ -1,3 +1,7 @@
 package main
 
-var Image []int = []int{0,0,0,0,255,1, 0,1,0,255,0,1}
+var Image []int = []int{
+//  x    y    r    g    b    a
+		0,   0,   0,   0,   255, 1,
+		0,   1,   0,   255, 0,   1,
+}
--- main.go
+++ main.go
@@ -5,6 +5,10 @@ import (
 	"github.com/kaey/framebuffer"
 )
 
+func devidable (a int, b int) bool {
+	return a % b == 0
+}
+
 func main () {
 	fb, err := framebuffer.Init("/dev/fb0")
 	if err != nil {
@@ -13,12 +17,36 @@ func main () {
 	}
 	defer fb.Close()
 
+	var w,h int = fb.Size()
+
 	if (len(Image) / 6) % 2 != 0 {
 		fmt.Print("Image length is not multiple of 6")
 	}
 
-	for i := 0; i < len(Image) / 6; i++ {
+	var Image2 []int
+	for i,e := range Image {
+		if devidable(i,1) {
+			if Image[i] < 0 {
+				Image2 = append(Image2, h - Image[i])
+			} else {
+				Image2 = append(Image2, Image[i])
+			}
+		} else if devidable(i,2) {
+			if Image[i] < 0 {
+				Image2 = append(Image2, w - Image[i])
+			} else {
+				Image2 = append(Image2, Image[i])
+			}
+		} else {
+			Image2 = append(Image2, Image[i])
+		}
+		if e < 0 {
+			Image2 = append(Image2, w-e)
+		}
+	}
+
+	for i := 0; i < len(Image2) / 6; i++ {
 		fb.Clear(0,0,0,0)
-		fb.WritePixel(i,i+1,i+2,i+3,i+4,i+5)
+		fb.WritePixel(Image[i],Image[i+1],Image[i+2],Image[i+3],Image[i+4],Image[i+5])
 	}
 }
